f723cfd264de2a81a5b694488bb1454c43cb475b
Nova034 <jcmg0304@gmail.com>
2024-04-22 22:14:30 -0400

    Agregando funcion z en func.py Agreggando funcion x en codigo2.py

--- codigo2.py
+++ codigo2.py
@@ -1,2 +1,5 @@
 # nuevo comentario
 # Aqui va el codigo de una funcion x
+def funcionX(a,b):
+    return a^b*a
+
--- func.py
+++ func.py
@@ -4,6 +4,8 @@ apellido = "Nova"
 saludo = "Hola " + nombre + " " + apellido + " como estas?"
 print(saludo)
 # Nuevo comentario sobre la funcion z
+def FuncionZ(z):
+    return z^z*z+z-z
 
 
 
