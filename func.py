8487f544b007494a3c0e8004b00bbb0fce1d8d27
Nova <94876626+Nova034@users.noreply.github.com>
2024-04-22 22:12:45 -0400

    Creando funcion z y agregando info al saludo en  func.py

--- func.py
+++ func.py
@@ -2,9 +2,10 @@ nombre = "Ruby"
 apellido = "Nova"
 
 saludo = "Hola " + nombre + " " + apellido + " como estas?"
-print(saludo)
+print(saludo + " XD")
 # Nuevo comentario sobre la funcion z
-
+def FuncionZ(z, z1, z2, z3):
+    return (z^z1) * (z2+z3-z1)
 
 
 
