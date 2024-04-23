e92f4823cf9a43ec64f5a41dfc52b12056f82efe
Nova034 <jcmg0304@gmail.com>
2024-04-22 22:55:53 -0400

    mamada

--- func.py
+++ func.py
@@ -2,11 +2,7 @@ nombre = "Ruby"
 apellido = "Nova"
 
 saludo = "Hola " + nombre + " " + apellido + " como estas?"
-print(saludo + " XD")
+print(saludo + " XD" + "mamada")
 # Nuevo comentario sobre la funcion z
 def FuncionZ(z, z1, z2, z3):
     return (z^z1) * (z2+z3-z1)
-
-
-
-
